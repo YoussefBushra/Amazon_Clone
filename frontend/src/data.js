62b38ab3b4a1d7f9e7f5e741919026b65eb90fd8
Youssef Bushra <youssef190346@bue.edu.eg>
2021-12-27 16:19:01 +0200

    Dynamic

---
+++ frontend/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Nike Slim shirt",
+            category: 'Shirts',
+            image: "./images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Best quality - Highly Rated Product"
+        },
+        {
+            _id: "2",
+            name: "Adidas fit shirt",
+            category: 'Shirts',
+            image: "./images/p2.jpg",
+            price: 100,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Best quality - Highly Rated Product"
+        },
+        {
+            _id: "3",
+            name: "Nike Slim shirt",
+            category: 'Shirts',
+            image: "./images/p3.jpg",
+            price: 99,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Best quality - Highly Rated Product"
+        },
+        {
+            _id: "4",
+            name: "Nike Slim shirt",
+            category: 'Shirts',
+            image: "./images/p4.jpg",
+            price: 129,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Best quality - Highly Rated Product"
+        },
+        {
+            _id: "5",
+            name: "Nike Slim shirt",
+            category: 'Shirts',
+            image: "./images/p5.jpg",
+            price: 75,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Best quality - Highly Rated Product"
+        },
+    ]
+};
+
+export default data;
